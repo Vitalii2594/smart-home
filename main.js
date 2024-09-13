c75b7708679095af6627a2442eced06fda8611c7
Vitalii Yakubovskyi <vitaliiyakubovskyi@gmail.com>
2024-09-13 14:52:45 +0200

    clr

--- main.js
+++ main.js
@@ -1,24 +1,80 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Obsługa kliknięcia burger menu
+document.querySelector('.burger').addEventListener('click', function() {
+  this.classList.toggle('active');
+  document.querySelector('.burger-menu-overlay').classList.toggle('active');
+});
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Obsługa kliknięcia przycisku zamknięcia
+document.querySelector('.close-btn').addEventListener('click', function() {
+  document.querySelector('.burger').classList.remove('active');
+  document.querySelector('.burger-menu-overlay').classList.remove('active');
+});
 
-setupCounter(document.querySelector('#counter'))
+// Zamknięcie menu po kliknięciu w link
+document.querySelectorAll('.burger-menu ul li a').forEach(link => {
+  link.addEventListener('click', function() {
+      document.querySelector('.burger').classList.remove('active');
+      document.querySelector('.burger-menu-overlay').classList.remove('active');
+  });
+});
+
+
+// TABS
+const tabsBtns = document.querySelectorAll(".tabs__nav button");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+// Ф-я скрывает табы и убирает active у кнопок
+function hideTabs() {
+  tabsItems.forEach((item) => item.classList.add("hide"));
+  tabsBtns.forEach((item) => item.classList.remove("active"));
+}
+
+// Ф-я показывает переданный номер таба и делает соответствующую ему кнопку активной.
+function showTab(index) {
+  tabsItems[index].classList.remove("hide");
+  tabsBtns[index].classList.add("active");
+}
+
+hideTabs();
+showTab(0);
+
+tabsBtns.forEach((btn, index) =>
+  btn.addEventListener("click", () => {
+    hideTabs();
+    showTab(index);
+  })
+);
+
+// Anchors
+const anchors = document.querySelectorAll(".header__nav a");
+
+anchors.forEach((anc) => {
+  anc.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const id = anc.getAttribute("href");
+    const elem = document.querySelector(id);
+
+    window.scroll({
+      top: elem.offsetTop - 80,
+      behavior: "smooth",
+    });
+  });
+});
+
+document.querySelector(".burger-menu").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".header__nav").classList.toggle("show");
+});
+
+// About us
+
+function openModal(element) {
+  var description = element.getAttribute("data-description");
+  document.getElementById("modal-description").textContent = description;
+  document.getElementById("modal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
